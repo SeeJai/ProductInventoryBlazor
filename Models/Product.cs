81db1f9d9e95e244235d0f7605d04cd8218300ef
Amal Ahammed C J <amalahammedcj7@gmail.com>
2026-06-08 16:58:23 +0530

    Build Blazor MongoDB product CRUD

---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ProductInventoryApp.Models;
+
+[BsonIgnoreExtraElements]
+public class Product
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("name")]
+    public string Name { get; set; } = string.Empty;
+
+    [BsonElement("price")]
+    public decimal Price { get; set; }
+
+    [BsonElement("quantity")]
+    public int Quantity { get; set; }
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,41 @@
+﻿using MongoDB.Driver;
+using ProductInventoryApp.Models;
+
+namespace ProductInventoryApp.Services;
+
+public class ProductService
+{
+    private readonly IMongoCollection<Product> _products;
+
+    public ProductService(IConfiguration configuration)
+    {
+        var connectionString = configuration["MongoDB:ConnectionString"];
+        var databaseName = configuration["MongoDB:DatabaseName"];
+        var collectionName = configuration["MongoDB:CollectionName"];
+
+        var client = new MongoClient(connectionString);
+        var database = client.GetDatabase(databaseName);
+
+        _products = database.GetCollection<Product>(collectionName);
+    }
+
+    public async Task<List<Product>> GetAllAsync()
+    {
+        return await _products.Find(_ => true).ToListAsync();
+    }
+
+    public async Task CreateAsync(Product product)
+    {
+        await _products.InsertOneAsync(product);
+    }
+
+    public async Task UpdateAsync(Product product)
+    {
+        await _products.ReplaceOneAsync(p => p.Id == product.Id, product);
+    }
+
+    public async Task DeleteAsync(string id)
+    {
+        await _products.DeleteOneAsync(p => p.Id == id);
+    }
+}
